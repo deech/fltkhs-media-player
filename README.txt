9c569dd283beb344b07c1c1e142de7507f84e7a9
Aditya Siram <aditya.siram@gmail.com>
2017-09-09 13:15:55 -0700

    More Win fixes

--- README.txt
+++
@@ -1,3 +0,0 @@
-A template for FLTKHS projects that use the Fluid UI builder.
-
-This template will always keep track with the latest version of FLTKHS on Hackage. http://hackage.haskell.org/package/fltkhs
--- win64-build.sh
+++ win64-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -d "vlc-2.2.6" ]; then
    wget http://download.videolan.org/pub/videolan/vlc/2.2.6/win64/vlc-2.2.6-win64.7z 
-   /c/Program\ Files/7-Zip/7z e -y vlc-2.2.6-win64.7z
+   /c/Program\ Files/7-Zip/7z x -y vlc-2.2.6-win64.7z
 fi
 
 stack build --flag fltkhs:bundled --extra-lib-dirs=$PWD/vlc-2.2.6 --extra-include-dirs=$PWD/vlc-2.2.6/sdk/include --force-dirty
